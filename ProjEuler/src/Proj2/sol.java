b3056e567f0ed87d442a37086986d86de11d5c46
matvince90 <msant080@fiu.edu>
2015-06-30 13:58:40 -0400

    changes to proj2

--- ProjEuler/src/Proj2/sol.java
+++ ProjEuler/src/Proj2/sol.java
@@ -1,4 +1,3 @@
-
 package Proj2;
 
 /**
@@ -6,9 +5,18 @@ package Proj2;
  * @author msant080
  */
 public class sol {
+    static int sol = 2;
     public static void main(String[] args){
-        
+        nextFibb(1,2);
     }
     
     
+    public static void nextFibb(int x, int y) {
+        int z = x + y;
+        if (z < 4000000) {
+            if (z%2 == 0)
+                sol += z;
+            nextFibb(y, z);
+        }
+    }
 }
