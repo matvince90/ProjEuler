da3462f71e7f4821837cb302c1f8e5612faec7ee
matvince90 <msant080@fiu.edu>
2015-06-30 13:45:41 -0400

    solution to problem 1
    created folder for problem 1 and 2, Finished problem 1

---
+++ ProjEuler/src/Proj1/sol.java
@@ -0,0 +1,46 @@
+package Proj1;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author msant080
+ */
+public class sol {
+    public static void main(String[] args) {
+        ArrayList<Integer> threes = getVals(3, 1000);
+        ArrayList<Integer> fives = getVals(5, 1000);
+        
+        System.out.println(threes);
+        System.out.println(fives);
+        
+        threes.addAll(fives);
+        Set<Integer> set = new HashSet<>(threes);
+        int sol = addSet(set);
+        
+        System.out.println("Answer: " + sol);
+        
+    }
+    
+    public static ArrayList<Integer> getVals(int check, int max) {
+        int counter = 1;
+        ArrayList<Integer> vals = new ArrayList<>();
+        while (counter < max) {
+            if ((counter)%check == 0)
+                vals.add(counter);
+            counter++;
+        }
+        return vals;
+    }
+    
+    public static int addSet(Set<Integer> arr) {
+        int ret = 0;
+        for(int i : arr) {
+            ret += i;
+        }
+        return ret;
+    }
+    
+}
---
+++ ProjEuler/src/Proj2/sol.java
@@ -0,0 +1,14 @@
+
+package Proj2;
+
+/**
+ *
+ * @author msant080
+ */
+public class sol {
+    public static void main(String[] args){
+        
+    }
+    
+    
+}
