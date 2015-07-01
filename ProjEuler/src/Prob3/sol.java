c0307fc1699c8fe478ae3db4fdb10769cea6e956
matvince90 <msant080@fiu.edu>
2015-06-30 21:38:32 -0400

    added Solution to Prob3

--- ProjEuler/src/Prob3/sol.java
+++ ProjEuler/src/Prob3/sol.java
@@ -5,10 +5,36 @@
  */
 package Prob3;
 
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author msant080
  */
 public class sol {
+    static long input;
+    static Set<Integer> factors ;
     
+    public static void main(String[] args) {
+        input = 600851475143L;
+        factors = new HashSet<>();
+        
+        getFactors(input);
+        System.out.println("Answer: " + factors);
+
+    }
+    
+    public static void getFactors(long num) {
+        long numCopy = num;
+        
+        for(int i = 2; i <= numCopy; i++) {
+            if (numCopy%i == 0) {
+                factors.add(i);
+                numCopy /= i;
+                i--;
+            }
+        }
+    }
 }
